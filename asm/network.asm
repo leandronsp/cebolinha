82c42469a067d40fccc4706c64eb9afc69361dac
Leandro Proença <leandronsp@gmail.com>
2025-08-13 18:11:42 -0300

    add network module

---
+++ asm/network.asm
@@ -0,0 +1,56 @@
+%include "asm/include/syscalls.inc"
+
+%define AF_INET 2
+%define SOCK_STREAM 1  
+%define SOCK_PROTOCOL 0
+%define BACKLOG 2
+
+section .data
+sockaddr:
+	sa_family: dw AF_INET   ; 2 bytes
+	port: dw 0xB80B         ; 2 bytes  
+	ip_addr: dd 0           ; 4 bytes
+	sin_zero: dq 0          ; 8 bytes
+
+section .bss
+sockfd: resb 8
+
+section .text
+global sockfd
+global create_socket
+global bind_socket
+global listen_socket
+global accept_connection
+
+create_socket:
+	mov rdi, AF_INET
+	mov rsi, SOCK_STREAM
+	mov rdx, SOCK_PROTOCOL
+	mov rax, SYS_socket
+	syscall
+	mov [sockfd], rax
+	ret
+
+bind_socket:
+	mov rdi, [sockfd]
+	mov rsi, sockaddr
+	mov rdx, 16
+	mov rax, SYS_bind
+	syscall
+	ret
+
+listen_socket:
+	mov rdi, [sockfd]
+	mov rsi, BACKLOG
+	mov rax, SYS_listen
+	syscall
+	ret
+
+accept_connection:
+	mov rdi, [sockfd]
+	mov rsi, 0
+	mov rdx, 0
+	mov r10, 0
+	mov rax, SYS_accept4
+	syscall
+	ret
--- asm/server.asm
+++ asm/server.asm
@@ -9,14 +9,15 @@ extern dequeue
 extern queuePtr
 extern queueSize
 extern queue
+extern sockfd
+extern create_socket
+extern bind_socket
+extern listen_socket
+extern accept_connection
 
 %include "asm/include/syscalls.inc"
 %include "asm/include/common.inc"
 
-%define AF_INET 2
-%define SOCK_STREAM 1
-%define SOCK_PROTOCOL 0
-%define BACKLOG 2
 %define CR 0xD
 %define LF 0xA
 
@@ -36,11 +37,6 @@ extern queue
 %define MAP_PRIVATE 0x0002      
 
 section .data
-sockaddr:
-	sa_family: dw AF_INET   ; 2 bytes
-	port: dw 0xB80B         ; 2 bytes
-	ip_addr: dd 0           ; 4 bytes
-	sin_zero: dq 0          ; 8 bytes
 response: 
 	headline: db "HTTP/1.1 200 OK", CR, LF
 	content_type: db "Content-Type: text/html", CR, LF
@@ -50,7 +46,6 @@ response:
 responseLen: equ $ - response
 
 section .bss
-sockfd: resb 8
 
 section .text
 _start:
@@ -73,34 +68,13 @@ _start:
 	je .socket
 	jmp .pool
 .socket:
-	; int socket(int domain, int type, int protocol)
-	mov rdi, AF_INET
-	mov rsi, SOCK_STREAM
-	mov rdx, SOCK_PROTOCOL
-	mov rax, SYS_socket
-	syscall
+	call create_socket
 .bind:
-	mov [sockfd], rax
-	; int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
-	mov rdi, [sockfd]
-	mov rsi, sockaddr
-	mov rdx, 16
-	mov rax, SYS_bind
-	syscall
+	call bind_socket
 .listen:
-	; int listen(int sockfd, int backlog)
-	mov rdi, [sockfd]
-	mov rsi, BACKLOG
-	mov rax, SYS_listen
-	syscall
+	call listen_socket
 .accept:
-	; int accept(int sockfd, struct *addr, int addrlen, int flags)
-	mov rdi, [sockfd]
-	mov rsi, 0
-	mov rdx, 0
-	mov r10, 0
-	mov rax, SYS_accept4
-	syscall
+	call accept_connection
 
 	mov r8, rax
 	call enqueue
