7689bc072741b8a973f84bbb5360face0570f1a2
Leandro Proença <leandronsp@gmail.com>
2025-08-16 19:56:52 -0300

    little refactoring

--- worker/config.go
+++ worker/config.go
@@ -15,11 +15,11 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		RedisPoolSize:       getEnvAsInt("WORKER_REDIS_POOL_SIZE", 10),
-		WorkerPoolSize:      getEnvAsInt("WORKER_POOL_SIZE", 10),
-		JobChannelSize:      getEnvAsInt("WORKER_JOB_CHANNEL_SIZE", 1000),
-		DefaultTimeoutMs:    getEnvAsInt("WORKER_DEFAULT_TIMEOUT_MS", 300),
-		FallbackTimeoutMs:   getEnvAsInt("WORKER_FALLBACK_TIMEOUT_MS", 100),
+		RedisPoolSize:       getEnvAsInt("WORKER_REDIS_POOL_SIZE", 50),
+		WorkerPoolSize:      getEnvAsInt("WORKER_POOL_SIZE", 50),
+		JobChannelSize:      getEnvAsInt("WORKER_JOB_CHANNEL_SIZE", 500),
+		DefaultTimeoutMs:    getEnvAsInt("WORKER_DEFAULT_TIMEOUT_MS", 3500),
+		FallbackTimeoutMs:   getEnvAsInt("WORKER_FALLBACK_TIMEOUT_MS", 300),
 	}
 }
 
